bc8c3687a36ff5e54973e9f27b58008dbd2fc065
Luis Sousa <lv-ss@hotmail.com>
2021-01-13 10:12:30 -0300

    recommit, por corrompimento de arquivos git

--- src/scripts/general/default.js
+++ src/scripts/general/default.js
@@ -1,7 +1,22 @@
+const menu = require('../modules/menu')
+const MiniCart = require('../modules/minicart')
+
 const Default = {
     init: function () {
-        console.log('testeeee')
+        Default.menuInit()
+        MiniCart.init('.header__carrinho')
+    },
+
+    menuInit: () => {
+        const menuConfig = {
+            tree: 3,
+            mainClass: 'header__nav-bar-list'
+        }
+ 
+        menu.init(menuConfig);
     }
+
+   
 }
 
 module.exports = Default
